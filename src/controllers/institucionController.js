7cf07642c39cd8635fffd81220b22711eaf20da6
Yeri Jimenez <yerijv@gmail.com>
2020-06-09 01:31:30 -0500

    Solucionando problema del modificar institucion

--- src/controllers/institucionController.js
+++ src/controllers/institucionController.js
@@ -99,8 +99,8 @@ controllers.modificar = async (req, res) => {
     const transaccion = await sequelize.transaction();
     const {ID, NOMBRE, INICIALES, IMAGEN, TELEFONO, PAGINA_WEB, UBICACION, DOMINIO, EXTENSION} = req.body.institucion;
     try {
-        if(IMAGEN){
-            let ruta = null
+        let ruta = null
+        if(IMAGEN){            
             fs.readFile(IMAGEN, function (err, data) {
                 let ruta = path.join("..","Imagenes","Institucion","logo."+EXTENSION)
                 fsPath.writeFile(ruta, data, function (err) {
